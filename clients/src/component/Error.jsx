433f5783d6fa8b58f183154fec611e8b1b1c6ab9
Rahulsainitech <rahuliim2001@gmail.com>
2023-07-29 22:01:05 +0530

    remove clients folder server

--- clients/src/component/Error.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Alert } from 'react-bootstrap';
-
-const Error = ({error}) => {
-  return <Alert variant="danger">{error}</Alert>
-}
-
-export default Error
--- clients/src/component/Loader.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { Spinner } from 'react-bootstrap'
-const Loader = () => {
-  return <Spinner animation="border" style={{position:"absolute",top:"45vh",left:"49vw"}} size="lg" variant="success" /> 
-}
-
-export default Loader
